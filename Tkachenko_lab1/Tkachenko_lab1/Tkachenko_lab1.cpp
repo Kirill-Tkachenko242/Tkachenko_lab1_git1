cd8d69ddd7c818cc5dd74d648538a9a1e01c5774
Kirill-Tkachenko242 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2022-09-20 19:32:54 +0300

    CRUD Pipe

---
+++ Tkachenko_lab1/Tkachenko_lab1/Tkachenko_lab1.cpp
@@ -0,0 +1,71 @@
+﻿
+
+
+#include <iostream>
+
+#include <vector>
+
+
+
+using namespace std;
+
+
+
+
+
+
+struct Pipe
+{
+    double lenght;
+    double diametr;
+    string repair;
+};
+
+
+
+Pipe InputPipe()
+{
+    Pipe s;
+    cout << "lenght:";
+    cin >> s.lenght;
+    cout << "diametr: ";
+    cin >> s.diametr;
+    cout << "repair: ";
+    cin >> s.repair;
+    return s;
+}
+
+void PrintPipe(const Pipe& s)
+{
+    cout << "lenght: " << s.lenght;
+    cout << "\tdiametr: " << s.diametr;
+    cout << "\trepair: " << s.repair << endl;
+}
+
+
+void EditPipe(Pipe& s)
+{
+    s.diametr -= 0.5;
+}
+
+void SavePipe(const Pipe& s)
+{
+
+    cout << "lenght: " << s.lenght;
+    cout << "\tdiametr: " << s.diametr;
+    cout << "\trepair: " << s.repair << endl;
+}
+
+
+
+
+int main()
+{
+    Pipe st = InputPipe();
+    PrintPipe(st);
+    EditPipe(st);
+    PrintPipe(st);
+    return 0;
+}
+
+
