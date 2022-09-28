9b97121c5854ceab2e3d94e69660a8fecbea20ff
Kirill-Tkachenko242 <110831941+Kirill-Tkachenko242@users.noreply.github.com>
2022-09-28 16:33:34 +0300

    menu

--- Tkachenko_lab1/Tkachenko_lab1/Tkachenko_lab1.cpp
+++ Tkachenko_lab1/Tkachenko_lab1/Tkachenko_lab1.cpp
@@ -22,14 +22,32 @@ struct Pipe
 };
 
 
+bool IsLenghtCorrect(double d)
+{
+    return d >= 2 && d <= 10000;
+}
+bool IsDiametrCorrect(double d)
+{
+    return d >= 2 && d <= 1000;
+}
 
 Pipe InputPipe()
 {
     Pipe s;
-    cout << "lenght:";
-    cin >> s.lenght;
-    cout << "diametr: ";
-    cin >> s.diametr;
+    do
+    {
+        cin.clear();
+        cin.ignore(100000, '\n');
+        cout << "lenght:";
+        cin >> s.lenght;
+    }while (cin.fail() || !IsLenghtCorrect(s.lenght));
+    do
+    {
+        cin.clear();
+        cin.ignore(100000, '\n');
+        cout << "diametr: ";
+        cin >> s.diametr;
+    }while (cin.fail() || !IsDiametrCorrect(s.diametr));
     cout << "repair: ";
     cin >> s.repair;
     return s;
@@ -45,36 +63,95 @@ void PrintPipe(const Pipe& s)
 
 Pipe LoadPipe()
 {
+    Pipe s;
     ifstream fin;
     fin.open("data.txt", ios::in);
-    Pipe s;
-    fin >> s.lenght;
-    fin >> s.diametr;
-    fin >> s.repair;
-    fin.close();
+    if (fin.is_open())
+    {
+        fin >> s.lenght;
+        fin >> s.diametr;
+        fin >> s.repair;
+        fin.close();
+    }
     return s;
 }
 
 void SavePipe(const Pipe& s)
 {
     ofstream fout;
-    fout.open("data.txt", 'w');
-    fout << s.lenght << endl << s.diametr << endl << s.repair << endl;
-    fout.close();
+    fout.open("data.txt", ios::out);
+    if (fout.is_open())
+    {
+        fout << s.lenght << endl 
+             << s.diametr << endl
+             << s.repair << endl;
+        fout.close();
+    }
 }
 
 
 void EditPipe(Pipe& s)
 {
     s.diametr -= 0.5;
+    s.diametr = IsDiametrCorrect(s.diametr) ? s.diametr:2;
+
+}
+
+void PrintMenu()
+{
+    cout << "1.Input pipe" << endl
+        << "2.Print pipe" << endl
+        << "3.Save to file" << endl
+        << "4.Load from file" << endl
+        << "5. Edit pipe" << endl
+        << "0. Exit" << endl;
 }
 
 int main()
 {
-    Pipe st = LoadPipe();
-    PrintPipe(st);
-    EditPipe(st);
-    SavePipe(st);
+    Pipe st;
+    while (1)
+    {
+        PrintMenu();
+        int i = 0;
+        cin >> i;
+        switch (i)
+        {
+        case 1:
+        {
+            st = InputPipe();
+            break;
+        }
+        case 2:
+        {
+            PrintPipe(st);
+            break;
+        }
+        case 3:
+        {
+            SavePipe(st);
+            break;
+        }
+        case 4:
+        {
+            st = LoadPipe();
+            break;
+        }
+        case 5:
+        {
+            EditPipe(st);
+            break;
+        }
+        case 0:
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Wrong action" << endl;
+        }
+        }
+    }
     return 0;
 }
 
